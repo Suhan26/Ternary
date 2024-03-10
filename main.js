5faaaabd13e522a75dcea9c2497909366d21c88d
suhan <md.suhanurrohanur2024@gmail.com>
2024-03-10 22:30:28 +0600

    f

---
+++ main.js
@@ -0,0 +1,20 @@
+var num1 = 36;
+
+var num2 = 33;
+
+var ruselt = num1 >  num2;
+
+// if (ruselt === true) {
+//     ruselt = num1 *2 + "Doble";
+// }
+
+// else{var ruselt = num1 + num2 + "Total";
+//      }
+
+// console.log(ruselt);
+
+
+
+ruselt === true ? ruselt = num1* 2 : ruselt = num1 + num2;
+
+console.log(ruselt);
